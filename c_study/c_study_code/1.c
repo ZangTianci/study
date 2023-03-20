4305c6950e9c6e8bd4acd598d4d75ac07558a189
ztc_magicdepth <2900139561@qq.com>
2023-03-20 17:14:21 +0800

    start study c

---
+++ c_study/c_study_code/1.c
@@ -0,0 +1,17 @@
+// 输入三个数a,b,c；不管怎么输入，输出的时候，abc由大到小顺序输出，用函数封装实现
+
+int sort(int *a, int *b, int *c){
+    int *p;
+    int 
+    
+
+}
+
+int main(){
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    sort(&a,&b,&c);
+}
